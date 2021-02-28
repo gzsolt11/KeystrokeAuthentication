3ec33f843b58fbfae054db6f52ba8c5b10066b28
zsolt <gergelyzsolt2001@gmail.com>
2021-02-28 17:57:25 +0200

    Creating keystroke authentication project

---
+++ KeystrokeAuthentication/src/main/java/com/example/keystrokeauthentication/KeystrokeAuthentication.kt
@@ -0,0 +1,10 @@
+package com.example.keystrokeauthentication
+
+import android.util.Log
+
+class KeystrokeAuthentication {
+
+    public fun makeLog(message:String){
+        Log.v("KeystrokeAuthentication",message)
+    }
+}
