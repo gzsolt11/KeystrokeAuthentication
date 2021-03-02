9e247b72f148a9108d4b692604a13fd839bad83f
zsolt <gergelyzsolt2001@gmail.com>
2021-03-02 12:13:04 +0200

    Fixing keystrokeauthenticationprovider class

--- KeystrokeAuthenticationModule/src/main/java/com/example/keystrokeauthentication/KeystrokeAuthenticationProvider.kt
+++ KeystrokeAuthenticationModule/src/main/java/com/example/keystrokeauthentication/KeystrokeAuthenticationProvider.kt
@@ -1,33 +1,27 @@
 package com.example.keystrokeauthentication
 
+import android.content.Context
 import android.content.Intent
-import android.os.Bundle
 import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
 
-class KeystrokeAuthenticationProvider: AppCompatActivity() {
+class KeystrokeAuthenticationProvider(var context: Context) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     public fun makeLog(message:String){
         Log.v("KeystrokeAuthentication",message)
-        startBackgroundProcess()
-        stopBackgroundProcess()
     }
 
     fun startBackgroundProcess(){
-        var intent = Intent(this, BackgroundProcess::class.java).also {
-            startService(it)
+        var intent = Intent(context, BackgroundProcess::class.java).also {
+            context.startService(it)
         }
         Log.v("KeystrokeAuthentication","background process started")
     }
 
     fun stopBackgroundProcess(){
-        var intent = Intent(this, BackgroundProcess::class.java).also {
-            stopService(it)
+        var intent = Intent(context, BackgroundProcess::class.java).also {
+            context.stopService(it)
         }
-        Log.v("KeystrokeAuthentication","background process stopped")
+        Log.v("KeystrokeAuthentication","background process stoppedBA")
     }
 }
