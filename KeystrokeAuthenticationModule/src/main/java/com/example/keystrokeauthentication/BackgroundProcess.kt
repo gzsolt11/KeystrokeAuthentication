4d847b18be2f7e505199ccccfcea050f33ae410b
zsolt <gergelyzsolt2001@gmail.com>
2021-03-09 15:01:57 +0200

    Adding the whole service and provider class to the repository

--- KeystrokeAuthenticationModule/src/main/java/com/example/keystrokeauthentication/BackgroundProcess.kt
+++ KeystrokeAuthenticationModule/src/main/java/com/example/keystrokeauthentication/BackgroundProcess.kt
@@ -2,37 +2,320 @@ package com.example.keystrokeauthentication
 
 import android.app.Service
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.IBinder
 import android.util.Log
-import java.io.FileReader
+import android.widget.Toast
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+
 
 class BackgroundProcess: Service() {
     val TAG = "BACKGROUNDPROCESS"
+    val TESTDATASIZE = 5
+    val TRAINDATASIZE = 5
+    var dataFrame = HashMap<Int, ArrayList<List<Int>>>()
+    var positiveTrainData: MutableList<List<Int>> = ArrayList()
+    var positiveTestData: MutableList<List<Int>> = ArrayList()
+    var negativeTestData: MutableList<List<Int>> = ArrayList()
+    var positiveTestScores: MutableList<Double> = ArrayList()
+    var negativeTestScores: MutableList<Double> = ArrayList()
+    var threshold: Double = 0.0
+    lateinit var pressedTimestamps: ArrayList<Long>
+    lateinit var releasedTimestamps: ArrayList<Long>
+    lateinit var authenticationPressedTimestamps: ArrayList<Long>
+    lateinit var authenticationReleasedTimestamps: ArrayList<Long>
 
-    init{
-        Log.v(TAG, "Service is running")
-    }
 
     override fun onBind(p0: Intent?): IBinder? = null
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        var extra = intent?.extras?.get("ID")
+        if (extra == 0) {
+            Log.v("VALASZ", "START")
+            if(isApplicationTrained()) {
+                Thread {
+                    dataFrame = readFile()
 
-        Thread {
-            while (true) {
-                readFile()
+                    val datas: ArrayList<MutableList<List<Int>>> = splitDataByUser(-1)
+
+                    positiveTrainData = datas[0]
+                    positiveTestData = datas[1]
+                    negativeTestData = datas[2]
+                    Log.v("TANITOTT TRAIN:",positiveTrainData.toString())
+                    Log.v("TANITOTT TEST:",positiveTestData.toString())
+
+                    positiveTestScores = manhattanScaledDistances(positiveTrainData, positiveTestData)
+                    negativeTestScores = manhattanScaledDistances(positiveTrainData, negativeTestData)
+
+                    val scores: ArrayList<MutableList<Double>> = ArrayList()
+                    scores.add(positiveTestScores)
+                    scores.add(negativeTestScores)
+
+                    positiveTestScores = scores[0]
+                    negativeTestScores = scores[1]
+
+                    threshold = getThreshold(positiveTestScores, negativeTestScores)
+                    Log.v("TANUL Threshold ", threshold.toString())
+                }.start()
             }
-        }.start()
+        }
+        if (extra == 1) {
+            Log.v("VALASZ", "autentikacio")
+            if(isApplicationTrained()) {
+                if(positiveTrainData.size != 0){
+                    Thread {
+                        authenticationPressedTimestamps = intent?.extras?.get("pressedTimestamps") as ArrayList<Long>
+                        authenticationReleasedTimestamps = intent?.extras?.get("releasedTimestamps") as ArrayList<Long>
+                        val authenticationKeystrokes = convertTimestampsToKeystrokes(authenticationPressedTimestamps, authenticationReleasedTimestamps)
+                        Log.v("AUTHENTICATION", authenticationKeystrokes.toString())
+                        val authenticateResult = authenticate(authenticationKeystrokes)
 
+                        val authIntent = Intent("SendResultToAuthenticationProvider")
+                        authIntent.putExtra("authenticationResult",authenticateResult)
+                        LocalBroadcastManager.getInstance(this.baseContext).sendBroadcast(authIntent)
+
+                        Log.v("TANULAS EREDMENY", authenticateResult.toString())
+                    }.start()
+                }else{
+                    Toast.makeText(applicationContext, "The service is not running!", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(applicationContext, "The application is not trained!", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        if (extra == 2) {
+            Log.v("VALASZ", "TRAIN")
+            Thread {
+                pressedTimestamps = intent?.extras?.get("pressedTimestamps") as ArrayList<Long>
+                releasedTimestamps = intent?.extras?.get("releasedTimestamps") as ArrayList<Long>
+                Log.v("GOT LISTS", pressedTimestamps.toString())
+                Log.v("GOT LISTS", releasedTimestamps.toString())
+
+                var inputTrainData = convertTimestampsToKeystrokes(pressedTimestamps, releasedTimestamps)
+                saveTrainDataToLocalSP(inputTrainData)
+
+            }.start()
+        }
         // if the server kills the service it will be recreated
         return START_REDELIVER_INTENT
     }
 
-    fun readFile(){
-        var text:String
-        val fileText = applicationContext.resources.openRawResource(R.raw.keylogs).bufferedReader().use { text = it.readText() }
+    fun readFile(): HashMap<Int, ArrayList<List<Int>>> {
+        var dataFrame = HashMap<Int, ArrayList<List<Int>>>()
+        applicationContext.resources.openRawResource(R.raw.keylogs).bufferedReader().use {
+            var text2 = it.lineSequence()
+            text2.iterator().forEach { it2 ->
+                var elements: MutableList<String> = it2.split(",") as MutableList<String>
+                if (elements[0].toIntOrNull() != null) {
+                    var userId = elements[elements.size - 1].toInt()
+                    elements.removeLast()
+                    //Log.v("READIN",elements.toString())
+                    var arrayList = dataFrame.get(userId)
+                    if (arrayList == null) {
+                        arrayList = ArrayList()
+                    }
+                    arrayList.add(elements.map { it -> it.toInt() })
+                    dataFrame.put(userId, arrayList)
+                }
+            }
+        }
 
-        Log.v("READFILE",text)
+        val sharedPreferences: SharedPreferences = getSharedPreferences("sharedPref", MODE_PRIVATE)
+        val gson = Gson()
+        val json = sharedPreferences.getString("trainData",null)
+        if(json == null){
+            Toast.makeText(applicationContext, "Application is not trained yet!", Toast.LENGTH_SHORT).show()
+        }else{
+            val type = object: TypeToken<ArrayList<List<Int>>>(){}.type
+            val trainData:ArrayList<List<Int>> = gson.fromJson(json,type)
+            Log.v("TANULNA",trainData.toString())
+            dataFrame.put(-1, trainData)
+        }
 
+
+        return dataFrame
+    }
+
+    fun splitDataByUser(userId: Int): ArrayList<MutableList<List<Int>>> {
+        val data = dataFrame[userId]
+        val datas: ArrayList<MutableList<List<Int>>> = ArrayList()
+        if (data != null) {
+            var positiveTrainData = data.subList(0, TRAINDATASIZE)
+            var positiveTestData = data.subList(5, TESTDATASIZE + TRAINDATASIZE)
+            var negativeTestData: MutableList<List<Int>> = ArrayList<List<Int>>()
+
+            for (key in dataFrame.keys) {
+                if (key != userId) {
+                    if (dataFrame[key] != null) {
+                        negativeTestData.add(dataFrame[key]!![0])
+                    }
+                }
+                if (negativeTestData.size == TESTDATASIZE) {
+                    break
+                }
+            }
+
+            datas.add(positiveTrainData)
+            datas.add(positiveTestData)
+            datas.add(negativeTestData)
+        }
+        return datas
 
     }
+
+    fun manhattanScaledDistances(positiveTrainData: MutableList<List<Int>>, testData: MutableList<List<Int>>): MutableList<Double> {
+        val meanVector = calculateMeanVector(positiveTrainData)
+        val madVector = calculateMadVector(positiveTrainData, meanVector)
+
+        //Log.v("TANULAS",positiveTrainData.toString())
+        //Log.v("TANULAS",meanVector.toString())
+        //Log.v("TANULAS",madVector.toString())
+
+        var testScores: MutableList<Double> = ArrayList()
+
+        for (i in 0 until testData.size) {
+            var score = 0.0
+            for (j in 0 until meanVector.size) {
+                score += abs(testData[i][j] - meanVector[j]) / madVector[j]
+            }
+            testScores.add(score)
+        }
+        testScores = testScores.map { it -> 1 / (1 + it) }.toMutableList()
+
+        return testScores
+    }
+
+    // atlag szamolasa minden oszlopra
+    fun calculateMeanVector(positiveTrainData: MutableList<List<Int>>): MutableList<Double> {
+        val meanVector: MutableList<Double> = ArrayList()
+        var sum = 0.0
+        for (i in 0..positiveTrainData[0].size - 1) {
+            sum = 0.0
+            for (k in 0 until positiveTrainData.size) {
+                sum += positiveTrainData[k][i]
+            }
+            meanVector.add(sum / positiveTrainData.size)
+        }
+        return meanVector
+    }
+
+    // atlagtol valo elteres atlaga
+    fun calculateMadVector(positiveTrainData: MutableList<List<Int>>, meanVector: MutableList<Double>): MutableList<Double> {
+        val madVector: MutableList<Double> = ArrayList()
+        var sum = 0.0
+        for (i in 0 until positiveTrainData[0].size) {
+            sum = 0.0
+            for (k in 0 until positiveTrainData.size) {
+                sum += abs(positiveTrainData[k][i] - meanVector[i])
+
+            }
+            madVector.add(sum / positiveTrainData.size)
+        }
+        return madVector
+    }
+
+    fun getThreshold(positiveTestScores: MutableList<Double>, negativeTestScores: MutableList<Double>): Double {
+        val allScores = ArrayList(positiveTestScores)
+        allScores.addAll(negativeTestScores)
+        val minScore = allScores.reduce { acc, next -> min(acc, next) }
+        val maxScore = allScores.reduce { acc, next -> max(acc, next) }
+        val unit: Double = (maxScore - minScore) / (TESTDATASIZE + TRAINDATASIZE)
+
+        val thresholds: MutableList<Double> = ArrayList()
+        var threshold = minScore
+
+        for (i in 0 until (TESTDATASIZE + TRAINDATASIZE)) {
+            thresholds.add(threshold)
+            threshold += unit
+        }
+        Log.v("THESHOLDS",thresholds.toString())
+
+        var savedThreshold = 0.0
+        var savedfn = positiveTestScores.size
+        for (thresholdElement in thresholds) {
+            val fp = negativeTestScores.filter { it -> it >= thresholdElement }.size
+            val fn = positiveTestScores.filter { it -> it < thresholdElement }.size
+
+            if ((fp == 0) and (fn < savedfn)) {
+                savedThreshold = thresholdElement
+                savedfn = fn
+            }
+        }
+        return savedThreshold
+    }
+
+    fun authenticate(keylog: MutableList<List<Int>>): Boolean {
+        val userScore = manhattanScaledDistances(positiveTrainData, keylog)
+        Log.v("USERSCORE",userScore.toString())
+        val count = userScore.filter { element -> element >= threshold }.size
+        if (count == keylog.size && keylog.size != 0) {
+            return true
+        }
+        return false
+    }
+
+    fun convertTimestampsToKeystrokes(pressedTimestamps: ArrayList<Long>, releasedTimestamps: ArrayList<Long>):ArrayList<List<Int>> {
+
+        Log.v("TANUL", pressedTimestamps.toString())
+        Log.v("TANUL", releasedTimestamps.toString())
+        var inputtedTrainData: ArrayList<List<Int>> = ArrayList()
+        var i = 0
+        while (i < pressedTimestamps.size) {
+            Log.v("TANUL", i.toString())
+            val H1 = (releasedTimestamps[i] - pressedTimestamps[i]).toInt()
+            val H2 = (releasedTimestamps[i + 1] - pressedTimestamps[i + 1]).toInt()
+            val H3 = (releasedTimestamps[i + 2] - pressedTimestamps[i + 2]).toInt()
+            val H4 = (releasedTimestamps[i + 3] - pressedTimestamps[i + 3]).toInt()
+            val H5 = (releasedTimestamps[i + 4] - pressedTimestamps[i + 4]).toInt()
+            val H6 = (releasedTimestamps[i + 5] - pressedTimestamps[i + 5]).toInt()
+
+            val PP1 = (pressedTimestamps[i + 1] - pressedTimestamps[i]).toInt()
+            val PP2 = (pressedTimestamps[i + 2] - pressedTimestamps[i + 1]).toInt()
+            val PP3 = (pressedTimestamps[i + 3] - pressedTimestamps[i + 2]).toInt()
+            val PP4 = (pressedTimestamps[i + 4] - pressedTimestamps[i + 3]).toInt()
+            val PP5 = (pressedTimestamps[i + 5] - pressedTimestamps[i + 4]).toInt()
+
+            val RP1 = (pressedTimestamps[i + 1] - releasedTimestamps[i]).toInt()
+            val RP2 = (pressedTimestamps[i + 2] - releasedTimestamps[i + 1]).toInt()
+            val RP3 = (pressedTimestamps[i + 3] - releasedTimestamps[i + 2]).toInt()
+            val RP4 = (pressedTimestamps[i + 4] - releasedTimestamps[i + 3]).toInt()
+            val RP5 = (pressedTimestamps[i + 5] - releasedTimestamps[i + 4]).toInt()
+
+            inputtedTrainData.add(listOf(H1, H2, H3, H4, H5, H6, PP1, PP2, PP3, PP4, PP5, RP1, RP2, RP3, RP4, RP5))
+
+            i += 6
+        }
+
+        return inputtedTrainData
+    }
+
+    private fun saveTrainDataToLocalSP(trainData: ArrayList<List<Int>>) {
+
+        val sharedPreferences: SharedPreferences = getSharedPreferences("sharedPref", MODE_PRIVATE)
+        Log.v("TANITOTT ADAT:",trainData.toString())
+        val editor = sharedPreferences.edit()
+        val gson = Gson()
+        val json = gson.toJson(trainData)
+        editor.putString("trainData",json)
+        editor.commit()
+
+    }
+
+    private fun isApplicationTrained():Boolean{
+        val sharedPreferences: SharedPreferences = getSharedPreferences("sharedPref", MODE_PRIVATE)
+        val gson = Gson()
+        val json = sharedPreferences.getString("trainData",null)
+        if(json == null){
+            Toast.makeText(applicationContext, "Application is not trained yet!", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
+    }
+
 }
--- KeystrokeAuthenticationModule/src/main/java/com/example/keystrokeauthentication/KeystrokeAuthenticationProvider.kt
+++ KeystrokeAuthenticationModule/src/main/java/com/example/keystrokeauthentication/KeystrokeAuthenticationProvider.kt
@@ -1,27 +1,287 @@
 package com.example.keystrokeauthentication
 
-import android.content.Context
-import android.content.Intent
+import android.app.Activity
+import android.app.Service
+import android.content.*
+import android.content.res.Resources
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.view.*
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.PopupWindow
+import android.widget.TextView
+import android.widget.Toast
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 
-class KeystrokeAuthenticationProvider(var context: Context) {
+
+class KeystrokeAuthenticationProvider(var activity: Activity, var editText: EditText) {
+
+    private var key0: TextView
+    private var key1: TextView
+    private var key2: TextView
+    private var key3: TextView
+    private var key4: TextView
+    private var key5: TextView
+    private var key6: TextView
+    private var key7: TextView
+    private var key8: TextView
+    private var key9: TextView
+    private var keyOk: TextView
+    private var keyDel: TextView
+    private lateinit var trainkey0: TextView
+    private lateinit var trainkey1: TextView
+    private lateinit var trainkey2: TextView
+    private lateinit var trainkey3: TextView
+    private lateinit var trainkey4: TextView
+    private lateinit var trainkey5: TextView
+    private lateinit var trainkey6: TextView
+    private lateinit var trainkey7: TextView
+    private lateinit var trainkey8: TextView
+    private lateinit var trainkey9: TextView
+    private lateinit var trainkeyOk: TextView
+    private lateinit var trainkeyDel: TextView
+    private lateinit var trainEditText: EditText
+    private lateinit var trainPinTitle: TextView
+    private lateinit var inputMethodManager:InputMethodManager
+    private lateinit var layoutInflater:LayoutInflater
+    private  var temporarPressedTimestamps:ArrayList<Long> = ArrayList()
+    private var temporarReleasedTimeStamps:ArrayList<Long> = ArrayList()
+    private  var pressedTimestamps:ArrayList<Long> = ArrayList()
+    private var releasedTimeStamps:ArrayList<Long> = ArrayList()
+    private  var authenticatePressedTimestamps:ArrayList<Long> = ArrayList()
+    private var authenticateReleasedTimestamps:ArrayList<Long> = ArrayList()
+    private var trainCounter = 10
+    private val TRAINCOUNTER = 10
+    //private var trainEditText: EditText
+    private var keyArray = ArrayList<TextView>()
+    private var trainKeyArray = ArrayList<TextView>()
+    private lateinit var keyboardPopUp:PopupWindow
+    private lateinit var trainPopUp:PopupWindow
+    private var isClicked = false
+    val sharedPreferences: SharedPreferences
+    var authenticationResult: Boolean? = null
+
+    init{
+        ruleEditText(editText)
+
+        key0 = keyboardPopUp.contentView.findViewById(R.id.buttonNumber0)
+        key1 = keyboardPopUp.contentView.findViewById(R.id.buttonNumber1)
+        key2 = keyboardPopUp.contentView.findViewById(R.id.buttonNumber2)
+        key3 = keyboardPopUp.contentView.findViewById(R.id.buttonNumber3)
+        key4 = keyboardPopUp.contentView.findViewById(R.id.buttonNumber4)
+        key5 = keyboardPopUp.contentView.findViewById(R.id.buttonNumber5)
+        key6 = keyboardPopUp.contentView.findViewById(R.id.buttonNumber6)
+        key7 = keyboardPopUp.contentView.findViewById(R.id.buttonNumber7)
+        key8 = keyboardPopUp.contentView.findViewById(R.id.buttonNumber8)
+        key9 = keyboardPopUp.contentView.findViewById(R.id.buttonNumber9)
+        keyArray.add(key0)
+        keyArray.add(key1)
+        keyArray.add(key2)
+        keyArray.add(key3)
+        keyArray.add(key4)
+        keyArray.add(key5)
+        keyArray.add(key6)
+        keyArray.add(key7)
+        keyArray.add(key8)
+        keyArray.add(key9)
+        keyOk = keyboardPopUp.contentView.findViewById(R.id.buttonOK)
+        keyDel = keyboardPopUp.contentView.findViewById(R.id.buttonDelete)
 
 
-    public fun makeLog(message:String){
-        Log.v("KeystrokeAuthentication",message)
+        setListenersToKeys()
+        sharedPreferences = activity.getSharedPreferences("sharedPref", Service.MODE_PRIVATE)
+
+        val myReciever:BroadcastReceiver = object : BroadcastReceiver(){
+            override fun onReceive(context: Context?, intent: Intent?) {
+                authenticationResult = intent!!.getBooleanExtra("authenticationResult",false)
+                Log.v("AUTHENTICATIONRESULT",authenticationResult.toString())
+                Toast.makeText(activity.applicationContext, "Authentication result:" + authenticationResult.toString(), Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
+        LocalBroadcastManager.getInstance(activity).registerReceiver(myReciever, IntentFilter("SendResultToAuthenticationProvider"))
+    }
+
+    private fun trainApplication(pressedTimestamps:ArrayList<Long>, releasedTimestamps:ArrayList<Long>){
+
+        var intent = Intent(activity.applicationContext, BackgroundProcess::class.java).also {
+            it.putExtra("ID", 2)
+            it.putExtra("pressedTimestamps",pressedTimestamps)
+            it.putExtra("releasedTimestamps",releasedTimestamps)
+            activity.applicationContext.startService(it)
+        }
     }
 
     fun startBackgroundProcess(){
-        var intent = Intent(context, BackgroundProcess::class.java).also {
-            context.startService(it)
+        var intent = Intent(activity.applicationContext, BackgroundProcess::class.java).also {
+            it.putExtra("ID", 0)
+            activity.applicationContext.startService(it)
         }
-        Log.v("KeystrokeAuthentication","background process started")
     }
 
-    fun stopBackgroundProcess(){
-        var intent = Intent(context, BackgroundProcess::class.java).also {
-            context.stopService(it)
+    fun authenticate(){
+        Log.v("BEIRT ADAT",authenticatePressedTimestamps.toString())
+        if(authenticatePressedTimestamps.size == 6) {
+            var intent = Intent(activity.applicationContext, BackgroundProcess::class.java).also {
+                it.putExtra("ID", 1)
+                it.putExtra("pressedTimestamps", authenticatePressedTimestamps)
+                it.putExtra("releasedTimestamps", authenticateReleasedTimestamps)
+                activity.applicationContext.startService(it)
+            }
+            authenticatePressedTimestamps = ArrayList()
+            authenticateReleasedTimestamps = ArrayList()
+            editText.setText("")
+
+        }else{
+            Toast.makeText(activity.applicationContext, "PIN is wrong!", Toast.LENGTH_SHORT).show()
         }
-        Log.v("KeystrokeAuthentication","background process stoppedBA")
     }
+
+
+    fun stopBackgroundProcess(){
+        var intent = Intent(activity.applicationContext, BackgroundProcess::class.java).also {
+            activity.applicationContext.stopService(it)
+        }
+    }
+
+    private fun ruleEditText(editText:EditText){
+        layoutInflater = activity.applicationContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        keyboardPopUp = PopupWindow(layoutInflater.inflate(R.layout.keyboard, null,false), Resources.getSystem().displayMetrics.widthPixels,ViewGroup.LayoutParams.WRAP_CONTENT,true)
+
+        inputMethodManager = activity.applicationContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(editText.rootView.findViewById(R.id.content), 0)
+        editText.isFocusable = false
+        editText.setOnClickListener{ view ->
+            keyboardPopUp.showAtLocation(activity.window.decorView.rootView,Gravity.BOTTOM,0,0)
+        }
+    }
+
+    fun train(){
+        pressedTimestamps = ArrayList()
+        releasedTimeStamps = ArrayList()
+        trainCounter = TRAINCOUNTER
+
+        trainPopUp = PopupWindow(layoutInflater.inflate(R.layout.trainingpincode, null,false), Resources.getSystem().displayMetrics.widthPixels,activity.window.decorView.height,true)
+        trainPopUp.showAtLocation(activity.window.decorView.rootView,Gravity.TOP,0,0)
+
+        trainPinTitle = trainPopUp.contentView.findViewById(R.id.trainPinCodeTitle)
+        trainPinTitle.text = "Write your pincode " + trainCounter + " times"
+
+        trainEditText = trainPopUp.contentView.findViewById<EditText>(R.id.trainPinCodeEditText)
+        inputMethodManager.hideSoftInputFromWindow(editText.rootView.findViewById(R.id.content), 0)
+        trainEditText.isFocusable = false
+
+        trainkey0 = trainPopUp.contentView.findViewById(R.id.buttonNumber0)
+        trainkey1 = trainPopUp.contentView.findViewById(R.id.buttonNumber1)
+        trainkey2 = trainPopUp.contentView.findViewById(R.id.buttonNumber2)
+        trainkey3 = trainPopUp.contentView.findViewById(R.id.buttonNumber3)
+        trainkey4 = trainPopUp.contentView.findViewById(R.id.buttonNumber4)
+        trainkey5 = trainPopUp.contentView.findViewById(R.id.buttonNumber5)
+        trainkey6 = trainPopUp.contentView.findViewById(R.id.buttonNumber6)
+        trainkey7 = trainPopUp.contentView.findViewById(R.id.buttonNumber7)
+        trainkey8 = trainPopUp.contentView.findViewById(R.id.buttonNumber8)
+        trainkey9 = trainPopUp.contentView.findViewById(R.id.buttonNumber9)
+        trainKeyArray.add(trainkey0)
+        trainKeyArray.add(trainkey1)
+        trainKeyArray.add(trainkey2)
+        trainKeyArray.add(trainkey3)
+        trainKeyArray.add(trainkey4)
+        trainKeyArray.add(trainkey5)
+        trainKeyArray.add(trainkey6)
+        trainKeyArray.add(trainkey7)
+        trainKeyArray.add(trainkey8)
+        trainKeyArray.add(trainkey9)
+        trainkeyOk = trainPopUp.contentView.findViewById(R.id.buttonOK)
+        trainkeyDel = trainPopUp.contentView.findViewById(R.id.buttonDelete)
+
+        setListenersToTrainKeys()
+
+    }
+
+
+
+    private fun setListenersToKeys(){
+        for(key in keyArray){
+            key.setOnClickListener{ view ->
+                if(editText.text.length < 6) {
+                    editText.setText(editText.text.toString() + key.text)
+                }
+            }
+            key.setOnTouchListener(object: View.OnTouchListener{
+                override fun onTouch(view: View?, event: MotionEvent?): Boolean {
+                    if(authenticateReleasedTimestamps.size < 6) {
+                        if (event?.action == MotionEvent.ACTION_DOWN && !isClicked) {
+                            isClicked = true
+                            authenticatePressedTimestamps.add(event.eventTime)
+                        }
+                        if (event?.action == MotionEvent.ACTION_UP && isClicked) {
+                            isClicked = false
+                            authenticateReleasedTimestamps.add(event.eventTime)
+                        }
+                    }
+                    return view?.onTouchEvent(event) ?: true
+                }
+            })
+        }
+        keyOk.setOnClickListener{ view ->
+            if(editText.text.length == 6) {
+                keyboardPopUp.dismiss()
+            }
+
+        }
+        keyDel.setOnClickListener{ view ->
+            editText.setText("")
+            authenticatePressedTimestamps = ArrayList()
+            authenticateReleasedTimestamps = ArrayList()
+        }
+    }
+
+    private fun setListenersToTrainKeys(){
+        for(key in trainKeyArray){
+            key.setOnClickListener{ view ->
+                if(trainEditText.text.length < 6) {
+                    trainEditText.setText(trainEditText.text.toString() + key.text)
+                }
+            }
+            key.setOnTouchListener(object: View.OnTouchListener{
+                override fun onTouch(view: View?, event: MotionEvent?): Boolean {
+                    if(temporarReleasedTimeStamps.size < 6) {
+                        if (event?.action == MotionEvent.ACTION_DOWN && !isClicked) {
+                            temporarPressedTimestamps.add(event.eventTime)
+                            isClicked = true
+                        }
+                        if (event?.action == MotionEvent.ACTION_UP  && isClicked) {
+                            temporarReleasedTimeStamps.add(event.eventTime)
+                            isClicked = false
+                        }
+                    }
+                    return view?.onTouchEvent(event) ?: true
+                }
+            })
+        }
+        trainkeyOk.setOnClickListener{ view ->
+            if(trainEditText.text.length == 6) {
+                pressedTimestamps.addAll(temporarPressedTimestamps)
+                releasedTimeStamps.addAll(temporarReleasedTimeStamps)
+                temporarPressedTimestamps = ArrayList()
+                temporarReleasedTimeStamps = ArrayList()
+                trainEditText.setText("")
+                trainCounter -= 1
+                trainPinTitle.text = "Write your pincode " + trainCounter + " times"
+                if (trainCounter == 0) {
+                    trainPopUp.dismiss()
+                    trainApplication(pressedTimestamps, releasedTimeStamps)
+                }
+            }
+        }
+        trainkeyDel.setOnClickListener{ view ->
+            trainEditText.setText("")
+            temporarPressedTimestamps = ArrayList()
+            temporarReleasedTimeStamps = ArrayList()
+        }
+    }
+
 }
